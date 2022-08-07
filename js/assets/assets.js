9dc5cb0ce60e6e2a5665ae1f907125d1411bb9f6
trungduc1012kk <trungduc1012kk@gmail.com>
2022-08-07 23:23:13 +0700

    ok

--- js/assets/assets.js
+++ js/assets/assets.js
@@ -5,6 +5,10 @@ class AssetPage {
     me.initEvent();
 
     me.initFormDetail();
+
+    me.pagination(pageNumber);
+
+    me.formatInput();
   }
 
   /**
@@ -30,38 +34,155 @@ class AssetPage {
   }
 
   /**
+   * hàm khởi tạo sự kiện cho Page Number
+   * Author: TTDuc (02/08/2022)
+   */
+  initEventPaging() {
+    let me = this;
+
+    //click vào paging__number
+    $(".paging .paging__number").off("click");
+    $(".paging .paging__number").on("click", function () {
+      pageNumber = parseInt($(this).text());
+      me.pagination(pageNumber);
+      console.log(pageNumber);
+    });
+
+    //click prev
+    $(".paging .paging__prev").off("click");
+    $(".paging .paging__prev").on("click", function () {
+      if (pageNumber > 1) {
+        pageNumber--;
+      }
+      console.log(pageNumber);
+      me.pagination(pageNumber);
+    });
+
+    //click next
+    $(".paging .paging__next").off("click");
+    $(".paging .paging__next").on("click", function () {
+      if (pageNumber < maxPageNumber) {
+        pageNumber++;
+      }
+      console.log(pageNumber);
+      me.pagination(pageNumber);
+    });
+  }
+
+  /**
+   * render Paging
+   * Author: TTDuc(02/08/2022)
+   */
+  pagination(pageNumber) {
+    let me = this;
+
+    //xóa tất cả phần tử con của paging
+    $(".paging").empty();
+    //render button prev
+    $(".paging").append(`<div class="paging__item paging__prev">
+                        <div class="icon">
+                          <div class="icon-prev"></div>
+                        </div>
+                      </div>`);
+
+    //render page 1
+    $(".paging").append(`<div class="paging__item paging__number">1</div>`);
+
+    //render more page button
+    if (pageNumber > 4) {
+      $(".paging").append(`<div class="paging__more1">...</div>`);
+    }
+
+    //render trang hiện tại và 2 trang liền kề
+    if (pageNumber > 4 && pageNumber < maxPageNumber - 3) {
+      $(".paging").append(`
+                            <div class="paging__item paging__number">${
+                              pageNumber - 1
+                            }</div>
+                            <div class="paging__item paging__number active">${pageNumber}</div>
+                            <div class="paging__item paging__number">${
+                              pageNumber + 1
+                            }</div>`);
+    } else if (pageNumber <= 4) {
+      $(".paging").append(`
+                            <div class="paging__item paging__number">2</div>
+                            <div class="paging__item paging__number">3</div>
+                            <div class="paging__item paging__number">4</div>`);
+    } else if (pageNumber >= maxPageNumber - 3) {
+      $(".paging").append(`
+                            <div class="paging__item paging__number">${
+                              maxPageNumber - 3
+                            }</div>
+                            <div class="paging__item paging__number">${
+                              maxPageNumber - 2
+                            }</div>
+                            <div class="paging__item paging__number">${
+                              maxPageNumber - 1
+                            }</div>`);
+    }
+
+    //render more page button
+    if (pageNumber < maxPageNumber - 3) {
+      $(".paging").append(`<div class="paging__more1">...</div>`);
+    }
+
+    //render max page
+    $(".paging").append(
+      `<div class="paging__item paging__number">${maxPageNumber}</div>`
+    );
+
+    //render button next
+    $(".paging").append(`<div class="paging__item paging__next">
+                        <div class="icon">
+                          <div class="icon-next"></div>
+                        </div>
+                      </div>`);
+
+    //xóa class active cũ
+    $(".paging .active").removeClass("active");
+    //thêm class active vào page hiện tại
+    $(".paging")
+      .find(".paging__number")
+      .each(function () {
+        if ($(this).text() == pageNumber) {
+          $(this).addClass("active");
+        }
+      });
+
+    me.initEventPaging();
+  }
+
+  /**
    * hàm khởi tạo sự kiện cho input-number
    * Author: TTDuc (01/08/2022)
    */
-  initEventInputNumber(){
+  initEventInputNumber() {
     let me = this;
 
     //click up tăng 1
-    $('.input-number .up-down .up').off("click");
-    $('.input-number .up-down .up').on("click", function(){
-        let inputVal = $(this).parents(".input-number").find("input").val();
+    $(".input-number .up-down .up").off("click");
+    $(".input-number .up-down .up").on("click", function () {
+      let inputVal = $(this).parents(".input-number").find("input").val();
 
-        inputVal = Number.parseFloat(inputVal) + 1;
+      inputVal = Number.parseFloat(inputVal) + 1;
 
-        $(this).parents(".input-number").find("input").val(inputVal);
+      $(this).parents(".input-number").find("input").val(inputVal);
     });
-
 
     //click down giảm 1
-    $('.input-number .up-down .down').off("click");
-    $('.input-number .up-down .down').on("click", function(){
-        let inputVal = $(this).parents(".input-number").find("input").val();
+    $(".input-number .up-down .down").off("click");
+    $(".input-number .up-down .down").on("click", function () {
+      let inputVal = $(this).parents(".input-number").find("input").val();
 
-        inputVal = Number.parseFloat(inputVal) - 1;
+      inputVal = Number.parseFloat(inputVal) - 1;
 
-        $(this).parents(".input-number").find("input").val(inputVal);
+      $(this).parents(".input-number").find("input").val(inputVal);
     });
-
   }
-
 
   /**
    * hàm khởi tạo sự kiện cho navbar
+   * Author: TTDuc(31/07/2022)
    */
   initEventNavbar() {
     let me = this;
@@ -132,12 +253,12 @@ class AssetPage {
     $(".combobox .drop-down .drop-down__item").off("click");
     $(".combobox .drop-down .drop-down__item").on("click", function () {
       let val = $(this).text();
+      //xét giá trị cho combobox
+      $(this).parents(".combobox").find(".input").val(val);
       //xóa class active trước đó
       $(this).parents(".drop-down").find(".active").removeClass("active");
       //thêm class active cho item được chọn
       $(this).addClass("active");
-      //xét giá trị cho combobox
-      $(this).parents(".combobox").find(".input").val(val);
     });
   }
 
@@ -153,6 +274,42 @@ class AssetPage {
 
     //sự kiện chọn tất cả item
     me.selectedAllItemTable();
+
+    //sự kiện click chuột phải
+    me.showContextMenu();
+
+    //sự kiện ẩn contextmenu
+    me.hideContextMenu();
+  }
+
+  /**
+   * sự kiện ẩn contextmenu
+   * Author: TTDuc(01/08/2022)
+   */
+  hideContextMenu() {
+    $("#contextMenu .overplay").off("click");
+    $("#contextMenu .overplay").on("click", function () {
+      $("#contextMenu").hide();
+    });
+  }
+
+  /**
+   * sự kiện click chuột phải vào table
+   * Author: TTDuc(01/8/2022)
+   */
+  showContextMenu() {
+    $("table tbody tr").off("contextmenu");
+    $("table tbody tr").on("contextmenu", function () {
+      event.preventDefault();
+      let x = event.clientX,
+        y = event.clientY;
+      $("#contextMenu .drop-down").css({
+        top: y,
+        left: x,
+      });
+      console.log(x + y);
+      $("#contextMenu").show();
+    });
   }
 
   /**
@@ -162,9 +319,8 @@ class AssetPage {
   selectedAllItemTable() {
     $(".th-checkbox input").off("click");
     $(".th-checkbox input").on("click", function () {
-
-        // input ở th được chọn thì chọn tất cả
-        if ($(this).prop("checked") == true) {
+      // input ở th được chọn thì chọn tất cả
+      if ($(this).prop("checked") == true) {
         $("tbody")
           .find(" .td-checkbox input")
           .each(function () {
@@ -172,8 +328,8 @@ class AssetPage {
             $(this).parents("tr").addClass("active");
           });
       }
-      // ngược lại thì hủy chọn tất cả 
-      else{
+      // ngược lại thì hủy chọn tất cả
+      else {
         $(this).prop("checked", false);
         $("tbody")
           .find(" .td-checkbox input")
@@ -193,11 +349,11 @@ class AssetPage {
     let me = this;
 
     $("#table tbody tr").off("click");
-    $("#table tbody tr").on("click", function () {
+    $("#table tbody tr").on("click" || "contextmenu", function () {
       if ($(this).hasClass("active")) {
         $(this).removeClass("active");
         $(this).find(".td-checkbox input").prop("checked", false);
-        $(".th-checkbox input").prop("checked", false)
+        $(".th-checkbox input").prop("checked", false);
       } else {
         $(this).find(".td-checkbox input").prop("checked", true);
         $(this).addClass("active");
@@ -224,7 +380,6 @@ class AssetPage {
     $("[commandType]").off("click");
     $("[commandType]").on("click", function () {
       let commandType = $(this).attr("commandType");
-      console.log(commandType);
       if (me[commandType] && typeof me[commandType] == "function") {
         me[commandType]($(this));
       }
@@ -249,7 +404,68 @@ class AssetPage {
 
     emplement.parents(".popup").hide();
   }
+
+  /**
+   * hàm xóa TS
+   * Author : TTDuc(04/08/2022)
+   */
+  delete(element) {
+    let me = this,
+      item = 0;
+    $("#table tbody")
+      .find(".active")
+      .each(function () {
+        item++;
+      });
+    if (item == 0) {
+    } else if (item == 1) {
+      let msg = "bạn có muốn xóa item này";
+
+      $("#form-confirm .form-confirm__text").text(msg);
+
+      $("#form-confirm").show();
+    } else {
+      let msg = item + " tài sản được chọn. bạn có muốn xóa không?";
+
+      $("#form-confirm .form-confirm__text").text(msg);
+
+      $("#form-confirm").show();
+    }
+    me.deleted();
+  }
+
+  /**
+   * hàm xóa thành công
+   * Author: TTDuc(05/08/2022)
+   */
+  deleted() {
+    $("#form-confirm .form-confirm__control .button__save").on("click");
+    $("#form-confirm .form-confirm__control .button__save").on(
+      "click",
+      function () {
+        console.log(1);
+        $("#form-notice").show();
+        $("#form-confirm").hide();
+
+        setTimeout(function () {
+          $("#form-notice").hide();
+        }, 2000);
+      }
+    );
+  }
+  /**
+   * hàm định dạng text
+   * Author: TTDuc(01/08/2022)
+   */
+  formatInput() {
+    $("[dataType=number]").each(function () {
+      $(this).addClass("text-right");
+    });
+  }
 }
 
 //tạo 1 biến cho trang Assets
+var maxPageNumber = 15;
+var pageNumber = 1;
+
 var assetPage = new AssetPage();
--- js/assets/formDetail.js
+++ js/assets/formDetail.js
@@ -1,33 +1,138 @@
-class FormDetail{
-    constructor(){
-        let me = this;
+class FormDetail {
+  constructor() {
+    let me = this;
 
-        me.initEvent();
+    me.initEvent();
+  }
 
-        me.initForm();
+  /**
+   * khởi tạo sự kiện cho trang
+   * Author: TTDuc (31/07/2022)
+   */
+  initEvent() {
+    let me = this;
+    //sự kiện combobox
+    me.initEventCombobox();
+    //sự kiện blur input
+    me.initEventBlur();
+  }
+
+  /**
+   * sự kiện cho combobox
+   * Author: TTDuc(04/08/2022)
+   */
+  initEventCombobox() {
+    $(".combobox .drop-down .drop-down__item").off("click");
+    $(".combobox .drop-down .drop-down__item").on("click", function () {
+      let code = $(this).find(".drop-down__code").text();
+      let name = $(this).find(".drop-down__name").text();
+      //xét giá trị cho combobox
+      $(this).parents(".combobox").find(".input").val(code);
+      //xóa class active trước đó
+      $(this).parents(".drop-down").find(".active").removeClass("active");
+      //thêm class active cho item được chọn
+      $(this).addClass("active");
+
+      let fieldName = $(this)
+        .parents(".combobox")
+        .find(".input")
+        .attr("fieldName");
+      console.log(name);
+      console.log(
+        $("#form-detail").find(`[${fieldName.slice(0, -4) + "Name"}]`)
+      );
+
+      $("#form-detail")
+        .find(`[fieldName=${fieldName.slice(0, -4) + "Name"}]`)
+        .val(name);
+    });
+  }
+
+  /**
+   * event Blur cho input
+   * Author: TTDuc(04/08/2022)
+   */
+  initEventBlur() {
+    let me = this;
+    $("#form-detail .row .input").blur(function () {
+      me.validateInput($(this));
+    });
+  }
+
+  /**
+   * hàm validate cho form
+   * Author:TTDuc(04/08/2022)
+   */
+  validateInput(element) {
+    let me = this,
+      isValid = me.validateRequired(element); //validate trường bắt buộc nhập
+
+    //validate các trường số number
+    if (isValid) {
+      isValid = me.validateNumber(element);
     }
 
-    /**
-     * khởi tạo sự kiện cho trang
-     * Author: TTDuc (31/07/2022)
-     */
-    initEvent(){
+    //validate ngày tháng
 
+    //validate độ dài
+
+    //validate
+  }
+
+  /**
+   * validate Number
+   * Author: TTDuc(04/08/2022)
+   */
+  validateNumber(element) {
+    let reg = /^-?\d*\.?\d*$/;
+    let isValid = true;
+    if (element.attr("dataType") == "number") {
+      if (!reg.test(element.val())) {
+        console.log(1);
+        element.parents(".row").find(".err-text").remove();
+        element
+          .parents(".row")
+          .append(`<p class="err-text">Vui lòng nhập đúng định dạng số</p>`);
+        element.css("border-color", "#e03232");
+        isValid = false;
+      } else {
+        element.parents(".row").find(".err-text").remove();
+        element.css("border-color", "#ccc");
+      }
     }
+    return isValid;
+  }
 
-    /**
-     * hàm khởi tạo formDetail
-     * Author: TTDuc (31/07/2022)
-     */
-    initForm(){
-
+  /**
+   * hàm khởi validate trường bắt buộc nhập
+   * Author: TTDuc(04/08/2022)
+   */
+  validateRequired(element) {
+    let isValid = true;
+    if (!element.val()) {
+      console.log(1);
+      element.parents(".row").find(".err-text").remove();
+      element
+        .parents(".row")
+        .append(`<p class="err-text">Bắt buộc nhập trường này</p>`);
+      element.css("border-color", "#e03232");
+      isValid = false;
+    } else {
+      element.parents(".row").find(".err-text").remove();
+      element.css("border-color", "#ccc");
     }
+    return isValid;
+  }
 
-    /**
-     * hàm mở formDetail
-     * Author: TTDuc(31/07/2022)
-     */
-    open(){
-        $('#form-detail').show();
-    }
+  /**
+   * hàm mở formDetail
+   * Author: TTDuc(31/07/2022)
+   */
+  open() {
+    let newCode = "TS12345";
+    $("#form-detail").show();
+    $("#AssetsCode").focus();
+    $("#AssetsCode").val(newCode);
+    $("[fieldName=YearFollow]").val($("#yearfolow").val());
+  }
 }
